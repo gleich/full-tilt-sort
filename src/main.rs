a3bf006906ab93ba2e5290efe0a5da2d2e963dd5
Matthew Gleich <git@mattglei.ch>
2021-03-14 22:21:01 -0400

    ✨ Generate the new file path
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 mod structure;
 
-fn main() { let files = structure::get_files().expect("Failed to get files in structure"); }
+fn main() {
+	let files = structure::get_files().expect("Failed to get files in structure");
+	for file in files.iter() {
+		let path = file.new_path();
+		println!("{}", &path.to_str().unwrap());
+	}
+}
--- src/structure.rs
+++ src/structure.rs
@@ -1,17 +1,28 @@
-use std::path::PathBuf;
-use std::time::SystemTime;
-use std::{env, fs};
+use std::path::{Path, PathBuf};
+use std::{env, fs, io};
 
+use chrono::{DateTime, Local};
 use walkdir::WalkDir;
 
 /// A file in the structure
 pub struct File {
 	path: PathBuf,
-	mod_time: SystemTime,
+	mod_time: DateTime<Local>,
+}
+
+impl File {
+	pub fn new_path(&self) -> PathBuf {
+		// Generating parts
+		let file_name = self.path.file_name().unwrap().to_str().unwrap();
+		let parent = DateTime::format(&self.mod_time, "%B/%A - %e");
+
+		// Put parts together
+		Path::new(&format!("{}/{}", parent, file_name)).to_path_buf()
+	}
 }
 
 /// Get all the file paths and their modification times
-pub fn get_files() -> std::io::Result<Vec<File>> {
+pub fn get_files() -> io::Result<Vec<File>> {
 	let mut files: Vec<File> = Vec::new();
 	let cwd = env::current_dir()?;
 
@@ -21,7 +32,10 @@ pub fn get_files() -> std::io::Result<Vec<File>> {
 
 		if meta.is_file() {
 			let mod_time = meta.modified()?;
-			files.push(File { path, mod_time })
+			files.push(File {
+				path,
+				mod_time: mod_time.into(),
+			})
 		}
 	}
 
